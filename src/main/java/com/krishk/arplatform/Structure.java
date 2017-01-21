021167942feea2519d18fe3a5a16d52892ebc5b4
krishkalai07 <krishkalai07@gmail.com>
2017-01-21 15:23:12 -0800

    Initial Commit

---
+++ src/main/java/com/krishk/arplatform/Structure.java
@@ -0,0 +1,268 @@
+package com.krishk.arplatform;
+
+import java.util.Arrays;
+import java.util.UUID;
+import java.util.Vector;
+import org.json.*;
+
+import static java.lang.Math.min;
+import static java.lang.Math.max;
+
+enum Type {
+    PLATFORM(0), STRUCTURE(1), BUILDING(2), FLOOR(3), ROOM(4);
+
+    private int value;
+    Type(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
+
+class GeoPoint {
+    private double latitude, longitude;
+
+    GeoPoint (String s_point) {
+        s_point = s_point.substring(1, s_point.length()-1);
+        String delims = "[,]";
+        String[] string_array = s_point.split(delims);
+
+        this.latitude = Double.parseDouble(string_array[0]);
+        this.longitude = Double.parseDouble(string_array[1]);
+    }
+
+    GeoPoint (String s_latitude, String s_longitude) {
+        this.latitude = Double.parseDouble(s_latitude);
+        this.longitude = Double.parseDouble(s_longitude);
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + latitude + ", " + longitude + ")";
+    }
+}
+
+@SuppressWarnings("ALL")
+class Structure {
+    private UUID id;
+    private String name;
+    private Type type;
+    private Vector<GeoPoint> polygon;
+    private int floor_height;
+    private String information;
+    private Vector<Structure> children_nodes;
+
+    Structure (Type type, String name, String polygon_data, int floor_height, String information) {
+        this.type = type;
+        this.name = name;
+        this.polygon = constructPolygon(polygon_data);
+        this.floor_height = floor_height;
+        this.information = information;
+        this.id = generateID();
+        this.children_nodes = new Vector<Structure>();
+
+    }
+
+    //
+    // Getters
+    //
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public Vector<GeoPoint> getPolygon() {
+        return polygon;
+    }
+
+    public int getFloorHeight() {
+        return floor_height;
+    }
+
+    public String getInformation() {
+        return information;
+    }
+
+    public Vector<Structure> getChildrenNodes() {
+        return children_nodes;
+    }
+
+    //
+    // Setters
+    //
+    public void setId() {
+        this.id = generateID();
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public void setPolygon(Vector<GeoPoint> polygon) {
+        this.polygon = polygon;
+    }
+
+    public void setFloorHeight(int floor_height) {
+        this.floor_height = floor_height;
+    }
+
+    public void setInformation(String information) {
+        this.information = information;
+    }
+
+    //
+    // Make Relation
+    //
+    void addChildNode(Structure node) {
+        if (node != null) {
+            children_nodes.add(node);
+        }
+    }
+
+    void removeChildNode(Structure node) {
+        if (node != null) {
+            children_nodes.remove(node);
+        }
+    }
+
+    //
+    // Utility functions
+    //
+    UUID generateID() {
+        return UUID.randomUUID();
+    }
+
+    private Vector<GeoPoint> constructPolygon (String polygon_data) {
+        Vector<GeoPoint> polygon = new Vector<GeoPoint>();
+        String[] tokens;
+        String delimiters = "[,]";
+
+        if (polygon_data.length() == 0){
+            return polygon;
+        }
+
+        tokens = polygon_data.split(delimiters);
+
+        for (int i = 0; i < tokens.length; i+=2) {
+            String latitude = tokens[i];
+            String longitude = tokens[i+1];
+
+            polygon.add(new GeoPoint(latitude, longitude));
+        }
+
+        return polygon;
+    }
+
+    private Vector<UUID> constructChildrenID (String id_list_string){
+        Vector<UUID> return_vector = new Vector<UUID>();
+        String[] tokens;
+        String delimiters = "[,]";
+
+        if (id_list_string.length() == 0){
+            return return_vector;
+        }
+
+        tokens = id_list_string.split(delimiters);
+
+        for (String token : tokens) {
+            return_vector.add(UUID.fromString(token));
+        }
+
+        return return_vector;
+    }
+
+    public boolean isInsidePolygon(GeoPoint point) {
+        int counter = 0;
+        int i;
+        double x_intercept;
+        GeoPoint p1, p2;
+
+        if (getPolygon().size() == 0) {
+            return true;
+        }
+
+        p1 = polygon.get(0);
+        for (i = 1; i < polygon.size(); i++) {
+            p2 = polygon.get(i%(polygon.size()-1));
+            if (point.getLongitude() > min(p1.getLongitude(), p2.getLongitude())) {
+                if (point.getLongitude() <= max(p1.getLongitude(), p2.getLongitude())) {
+                    if (point.getLatitude() <= max(p1.getLatitude(), p2.getLatitude())) {
+                        if (p1.getLongitude() != p2.getLongitude()) {
+                            x_intercept = (point.getLongitude() - p1.getLongitude()) * (p2.getLatitude() - p1.getLatitude()) / (p2.getLongitude() - p1.getLongitude()) + p1.getLatitude();
+                            if (p1.getLatitude() == p2.getLatitude() || point.getLatitude() <= x_intercept) {
+                                counter++;
+                            }
+                        }
+                    }
+                }
+            }
+            p1 = p2;
+        }
+        return counter % 2 != 0;
+    }
+
+    @Override
+    public String toString() {
+        String ret_value = "";
+
+        ret_value += "ID = " + id + "\n";
+        ret_value += "Name = " + name + "\n";
+        ret_value += "Type = " + type + "\n";
+        ret_value += "Polygon = " + polygon.toString() + "\n";
+        ret_value += "Floor_height = " + floor_height + "\n";
+        ret_value += "information = " + information + "\n";
+        ret_value += "Children nodes = " + children_nodes + "\n";
+
+        return ret_value;
+    }
+
+    String toJSON() {
+        JSONObject return_object = new JSONObject();
+        return_object.put("Type", type.getValue());
+        return_object.put("ID", id);
+        return_object.put("Name", name);
+
+        String polygon_string = "";
+        for (int i = 0; i < polygon.size(); i++) {
+            polygon_string += polygon.get(i).getLatitude() + ", " + polygon.get(i).getLongitude();
+            if (i < polygon.size()-1) {
+                polygon_string += ",";
+            }
+        }
+        return_object.put("Polygon", polygon_string);
+        return_object.put("FloorHeight", floor_height);
+        return_object.put("Information", information);
+
+        String id_string = "";
+        for (int i = 0; i < children_nodes.size(); i++) {
+            id_string += children_nodes.get(i).getId();
+            if (i < children_nodes.size()-1) {
+                id_string += ",";
+            }
+        }
+        return_object.put("Children IDs", id_string);
+
+        return return_object.toString();
+    }
+}
---
+++ src/main/java/com/krishk/arplatform/Structures.java
@@ -0,0 +1,63 @@
+package com.krishk.arplatform;
+
+import java.util.Vector;
+
+@SuppressWarnings("ALL")
+public class Structures {
+    private Structure platform_node; //Root node of the tree
+    private Vector<String> json_list = new Vector<>();
+
+    Structures() {
+        this.platform_node = new Structure(Type.PLATFORM, "Structures", "", 0, "");
+
+        String polygon_data = "37.388616, -122.110888, 37.388610, -122.107366, 37.385015, -122.107379, 37.385034, -122.110901, 37.388616, -122.110888";
+        Structure los_altos_high_school = new Structure(Type.STRUCTURE, "Los Altos high School", polygon_data, 0, "Mah school");
+        platform_node.addChildNode(los_altos_high_school);
+
+        polygon_data = "37.385647, -122.109474, 37.385727, -122.109137, 37.385686, -122.109118, 37.385704, -122.109037, 37.385587, -122.108991, 37.385599, -122.108940, 37.385519, -122.108905, 37.385536, -122.108825, 37.385417, -122.108779, 37.385364, -122.108982, 37.385406, -122.108995, 37.385337, -122.109252, 37.385374, -122.109271, 37.385354, -122.109360, 37.385647, -122.109474";
+        Structure eagle_theater = new Structure(Type.BUILDING, "Eagle Theater", polygon_data, 0, "Mah school");
+        los_altos_high_school.addChildNode(eagle_theater);
+
+        polygon_data = "37.386006, -122.109427, 37.386649, -122.109426, 37.386652, -122.109093, 37.386463, -122.109093, 37.386463, -122.109030, 37.386149, -122.109028, 37.386152, -122.108939, 37.386007, -122.108936, 37.386006, -122.109427";
+        Structure wing700 = new Structure(Type.BUILDING, "700 Wing", polygon_data, 0, "Science and math buildings");
+        los_altos_high_school.addChildNode(wing700);
+
+        polygon_data = "";
+        Structure wing700_floor1 = new Structure(Type.FLOOR, "Science floor", polygon_data, 10, "All science buildings are on this floor");
+        wing700.addChildNode(wing700_floor1);
+
+        polygon_data = "";
+        Structure wing700_floor2 = new Structure(Type.FLOOR, "Math floor", polygon_data, 11, "All math buildings are on this floor");
+        wing700.addChildNode(wing700_floor2);
+
+        //polygon_data = ""; //TODO: needs to be created. maps.google.com is not accurate enough
+        //Structure wing700_floor2_room710 = new Structure(Type.ROOM, "Room 710", polygon_data, 11, "Mr.Dressen's Room");
+        //wing700_floor1.addChildNode(wing700_floor2_room710);
+
+    }
+
+    public Vector<String> getJsonList() {
+        return json_list;
+    }
+
+    void locateStructure(GeoPoint point) {
+        System.out.println("Is inside locate.");
+        locateStructuresHelper(point, platform_node);
+    }
+
+    private void locateStructuresHelper(GeoPoint point, Structure node) {
+        // Base case
+        if (node == null) {
+            return;
+        }
+
+        //System.out.println("Testing " + node.getName());
+        if (node.isInsidePolygon(point)) {
+            System.out.println("Found inside " + node.getName());
+            json_list.add(node.toJSON());
+            for (int i = 0; i < node.getChildrenNodes().size(); i++) {
+                locateStructuresHelper(point, node.getChildrenNodes().get(i));
+            }
+        }
+    }
+}
