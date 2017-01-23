b1c1c7e75a1959f1cf654ef536cd869052a12c9b
krishkalai07 <krishkalai07@gmail.com>
2017-01-22 23:38:12 -0800

    Optimizing Logic

--- src/main/java/com/krishk/arplatform/Structures.java
+++ src/main/java/com/krishk/arplatform/Structures.java
@@ -72,6 +72,9 @@ public class Structures {
         }
     }
 
+    /**
+     *
+     */
     public void handleOutsideStructures() {
         for (Structure structure : platform_node.getChildrenNodes()) {
             json_list.add(structure.toJSON());
