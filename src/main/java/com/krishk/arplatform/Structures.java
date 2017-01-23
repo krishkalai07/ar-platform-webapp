542504aa867d55adddb42eaac35f5abbe6d1d09c
krishkalai07 <krishkalai07@gmail.com>
2017-01-22 23:31:36 -0800

    Optimizing Logic

--- src/main/java/com/krishk/arplatform/Structures.java
+++ src/main/java/com/krishk/arplatform/Structures.java
@@ -93,9 +93,16 @@ public class Structures {
             md.update(etag_string.getBytes());
             byte[] digest = md.digest();
 
-            System.out.println(Arrays.toString(digest));
+            StringBuilder hexString = new StringBuilder();
 
-            Etag = new String(digest);
+            for (int i = 0; i < digest.length; i++) {
+                String hex = Integer.toHexString(0xFF & digest[i]);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+            Etag = hexString.toString();
         } catch (NoSuchAlgorithmException e) {
             System.out.println("MD5 failed me");
         }
