d496991dcc839816dabe1acd6a03cab89035a014
krishkalai07 <krishkalai07@gmail.com>
2017-02-22 14:56:44 -0800

    output tree

--- src/main/java/com/krishk/arplatform/ARTree.java
+++ src/main/java/com/krishk/arplatform/ARTree.java
@@ -32,8 +32,9 @@ public class ARTree {
      */
     ARTree() {
         this.rootNode = new ARNode(Type.PLATFORM, "Structures", "", 0, "");
-
-        String polygon_data = "37.388616, -122.110888, 37.388610, -122.107366, 37.385015, -122.107379, 37.385034, -122.110901, 37.388616, -122.110888";
+        String polygon_data = "";
+/*
+        polygon_data = "37.388616, -122.110888, 37.388610, -122.107366, 37.385015, -122.107379, 37.385034, -122.110901, 37.388616, -122.110888";
         ARNode los_altos_high_school = new ARNode(Type.STRUCTURE, "Los Altos high School", polygon_data, 0, "Mah school");
         rootNode.addChildNode(los_altos_high_school);
 
@@ -56,7 +57,7 @@ public class ARTree {
         polygon_data = "37.38642893, -122.10951252, 37.38649573, -122.10958477, 37.38646007,-122.10937816, 37.38648869,-122.10947966, 37.38643915,-122.10937413, 37.38642285,-122.10940775, 37.38640919,-122.10934203, 37.38642432,-122.10935737, 37.38642893,-122.10951252";
         ARNode wing700_floor1_room710 = new ARNode(Type.ROOM, "Room 710", polygon_data, 11, "Mr.Dressen's Room");
         wing700_floor1.addChildNode(wing700_floor1_room710);
-/*
+        */
         //House
         polygon_data = "37.404176,-122.078845,37.404179,-122.078965,37.404125,-122.078970,37.404118,-122.078849,37.404176,-122.078845";
         ARNode house = new ARNode(Type.STRUCTURE, "House", polygon_data, 0, "A House");
@@ -69,7 +70,7 @@ public class ARTree {
         polygon_data = "37.404238, -122.078921, 37.404178, -122.078923, 37.404122, -122.078969, 37.404179, -122.078965, 37.404238, -122.078921";
         ARNode living_room = new ARNode(Type.ROOM, "Living Room", polygon_data, 0, "Living Room");
         house.addChildNode(house);
-*/
+
         constructStructuresData();
     }
 
